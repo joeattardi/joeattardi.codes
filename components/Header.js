63c04c84bdbdfcf2bfde91d24192c16aaa051a84
Joe Attardi <jattardi@gmail.com>
2020-07-27 23:07:32 -0400

    Add skeleton landing page

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,6 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles['logo-container']}>
-        <img src="/joe-memoji.jpg" alt="Joe Attardi" />
-      </div>
       <h1>Joe Attardi</h1>
       <HeaderMenu />
     </header>
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,15 @@ import styles from './index.module.css';
 export default function Home() {
   return (
     <Layout>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles.hero}>
+          <img src="/joe.jpg" />
+          <div>
+            <h1>Joe Attardi</h1>
+            <h2>Full Stack Software Engineer</h2>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
